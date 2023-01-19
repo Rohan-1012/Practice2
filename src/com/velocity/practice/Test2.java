c8a762f09c67ab11d63833e38fdd209b1c20e969
Rohan <rohankhopade001@gmail.com>
2023-01-19 14:16:34 +0530

    Added new program

---
+++ src/com/velocity/practice/Test2.java
@@ -0,0 +1,23 @@
+package com.velocity.practice;
+
+import java.util.Scanner;
+
+public class Test2 {
+
+	public static void main(String[] args) {
+		
+	Scanner sc = new Scanner(System.in);
+	System.out.println("Enter the no");
+	int num =sc.nextInt();
+	
+	StringBuffer rev;
+	StringBuffer sb = new StringBuffer(String.valueOf(num));
+	 rev =sb.reverse();
+	
+		
+		
+		System.out.println(rev);
+
+	}
+
+}
